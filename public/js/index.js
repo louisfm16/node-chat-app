f73a0f763dfb575a8eef75637af911d4118d4bb7
Louis Medina <louisfm16@gmail.com>
2018-10-18 18:05:16 -0700

    Great new User + Alerts others

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
+// ES6 Arrow functions are not supported in most browsers
 var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server!');
+
+    socket.on('newUser', function(newUser) {
+        console.log('newUser', newUser);
+    });
 
     socket.on('newMessage', function(newMessage) {
         console.log('newMessage', newMessage);
--- server/server.js
+++ server/server.js
@@ -15,14 +15,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit from admin text Welcome to the chat app
+    socket.emit('newUser', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newUser', {
+        from: 'Admin',
+        text: 'New user _______ has joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (msg) => {
         console.log('New message created!');
 
+        // Sends event to all clients
         io.emit('newMessage', {
             from: msg.from,
             text: msg.text,
             createdAt: new Date().getTime()
         });
+
+        // Sends event to all except sender
+        // socket.broadcast.emit('newMessage', {
+        //     from: msg.from,
+        //     text: msg.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
